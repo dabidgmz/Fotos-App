5b716f671104c1ed3bcab0f487ee410ead384199
David Herrera <david.gmzherrera28@gmail.com>
2025-05-08 20:45:11 -0600

    Initial Commit

---
+++ Fotos-App/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Fotos-App
+//
+//  Created by david gomez herrera on 06/05/25.
+//
+
+import UIKit
+
+class GaleriaViewController: UIViewController
+
+{
+    @IBOutlet weak var SrcFoto: UIScrollView!
+    
+    @IBOutlet weak var ImbFoto: UIImageView!
+    
+    @IBOutlet weak var BtnAnterior: UIButton!
+    
+    
+    @IBOutlet weak var BtnSiguiente: UIButton!
+    
+    @IBOutlet weak var LblContador: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    @IBAction func IrAnterior() {
+    }
+    
+    
+    @IBAction func IrSiguiente() {
+    }
+    
+    
+    @IBAction func mostrarMenu() {
+        let menu  = UIAlertController(title: "Menu de Opciones", message: "Selecciona una opcion :", preferredStyle: .actionSheet)
+        let camara = UIAlertAction(title: "Tomar Foto", style: .default) { accion in
+            self.tomarFotoCamara()
+        }
+        let galeria = UIAlertAction(title:"Foto de la galeria", style: .default) {accion in
+            self.seleccionarFotoGaleria()
+        }
+        let guardar = UIAlertAction(title: "Guardar Foto en galeria", style: .default){accion in
+            self.guardarFotoGaleria()
+        }
+        
+        let cancelar = UIAlertAction(title: "Cnacelar", style: .cancel)
+        
+        menu.addAction(camara)
+        menu.addAction(galeria)
+        menu.addAction(guardar)
+        menu.addAction(cancelar)
+        present(menu, animated: true)
+        
+    }
+    
+    func tomarFotoCamara(){
+        let camara = UIImagePickerController()
+        camara.sourceType = .camera
+        present(camara, animated: true)
+        
+    }
+    
+    func seleccionarFotoGaleria(){
+        
+    }
+    
+    func guardarFotoGaleria(){
+        
+    }
+    
+}
+
