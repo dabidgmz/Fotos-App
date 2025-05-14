bd5f664c3daaf39bc2524ae18fb615a1937f78bc
David Herrera <david.gmzherrera28@gmail.com>
2025-05-13 18:38:28 -0600

    Actualizacion de cambion en ViewController

--- Fotos-App/ViewController.swift
+++ Fotos-App/ViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import PhotosUI
 
 class GaleriaViewController: UIViewController,UIImagePickerControllerDelegate,
-                             UINavigationControllerDelegate
+                             UINavigationControllerDelegate,UIScrollViewDelegate, PHPickerViewControllerDelegate
 
 {
+    
+    
     @IBOutlet weak var SrcFoto: UIScrollView!
     
     @IBOutlet weak var ImbFoto: UIImageView!
@@ -23,18 +26,27 @@ class GaleriaViewController: UIViewController,UIImagePickerControllerDelegate,
     @IBOutlet weak var LblContador: UILabel!
     
     
+    var foto: [UIImage] = []
+    var indice = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return ImbFoto
+    }
     
     @IBAction func IrAnterior() {
+        indice -= 1
+        setFotos()
     }
     
     
     @IBAction func IrSiguiente() {
+        indice += 1
+        setFotos()
     }
     
     
@@ -72,15 +84,64 @@ class GaleriaViewController: UIViewController,UIImagePickerControllerDelegate,
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         dismiss(animated: true)
-        print(info[.originalImage] as! UIImage)
-    }
-    func seleccionarFotoGaleria(){
+        foto.append(info[.originalImage] as! UIImage)
+        if !foto.isEmpty{
+            indice = foto.count - 1;
+        }
+        setFotos();
         
     }
+    
+    func setFotos(){
+        ImbFoto.image = foto[indice]
+        LblContador.text = "\(indice+1)/\(foto.count)"
+        
+        if indice == 0{
+            BtnAnterior.isHidden = true;
+        }
+        else{
+            BtnAnterior.isHidden = false;
+        }
+        
+        if indice+1 == foto.count{
+            
+            BtnSiguiente.isHidden = true;
+        }
+        else{
+            BtnSiguiente.isHidden = false;
+        }
+        let escalaW = SrcFoto.frame.width / foto[indice].size.width
+        let escalaH = SrcFoto.frame.height / foto[indice].size.height
+        let escala = min(escalaH, escalaW)
+        SrcFoto.minimumZoomScale = escala
+        SrcFoto.maximumZoomScale = escala * 20
+        SrcFoto.zoomScale = escala
+        
+    }
+    
+    func seleccionarFotoGaleria(){
+        
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 2
+        config.filter = .images
+        
+        let galeria = PHPickerViewController(configuration: config)
+        galeria.delegate = self
+        present(galeria, animated: true)
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        for resultado in results {
+            resultado.itemProvider.loadObject(ofClass: UIImage.self) { objeto, error in
+                print("Error al cargar imagen: \(String(describing: error?.localizedDescription))")
+            }
+        }
+        dismiss(animated: true)
+    }
+
     
     func guardarFotoGaleria(){
         
     }
     
 }
-
